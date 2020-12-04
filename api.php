c421dc514effc5a2ebc2152cd2433289babcb2e8
Muhammad Raihan Permadi <32005026+rpd-251003@users.noreply.github.com>
2020-12-04 10:41:05 +0700

    Create api.php

---
+++ api.php
@@ -0,0 +1,18 @@
+<?php
+$data = $_GET['url'];
+$proses = likes_count($data);
+echo $proses;
+
+function followers_count($data){
+    $id = file_get_contents("https://instagram.com/web/search/topsearch/?query=".$data);
+    $id = json_decode($id, true);
+    $count = $id['users'][0]['user']['follower_count'];
+    return $count;
+}
+
+function likes_count($data){
+    $id = file_get_contents("".$data."?&__a=1");
+    $id = json_decode($id, true);
+    $count = $id['graphql']['shortcode_media']['edge_media_preview_like']['count'];
+    return $count;
+}
