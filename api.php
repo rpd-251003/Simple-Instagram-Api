d28ff692c4f1e91d310686b0f026264242fda9c5
Muhammad Raihan Permadi <32005026+rpd-251003@users.noreply.github.com>
2022-06-17 18:41:11 +0700

    Update api.php
    Fixing Bug.

--- api.php
+++ api.php
@@ -1,29 +1,29 @@
 <?php
 session_start();
+
 $data = $_GET['url'];
 $type = $_GET['type'];
 
-if(!empty($type == 1)){
-    // url = username instagram
-    echo followers_count($data);
-} else if($type == 2) {
-    // url = link post instagram
-    echo likes_count($data);
-} else {
-    echo 'Sorry your request is invalid';
-}
-
-function followers_count($data){
+function followers_count($data)
+{
     $id = file_get_contents("https://www.instagram.com/".$data."/?&__a=1");
     $id = json_decode($id, true);
     $count = $id['graphql']['user']['edge_followed_by']['count'];
     return $count;
 }
 
-function likes_count($data){
+function likes_count($data)
+{
     $id = file_get_contents($data."?/&__a=1");
     $id = json_decode($id, true);
     $count = $id['items'][0]['like_count'];
     return $count;
 }
-?>
+
+if(isset($type == 1)){
+    echo followers_count($data);
+} else if(isset($type == 2)) {
+    echo likes_count($data);
+} else {
+    echo 'Sorry your request is invalid';
+}
